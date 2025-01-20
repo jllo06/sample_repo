cf80ba693b81952c7d810472062345a2f0a19588
Lovely Salve <salvelynz@gmail.com>
2025-01-20 22:46:16 +0800

    add F01

--- SAM-BE/index.php
+++ SAM-BE/index.php
@@ -1,19 +1,6 @@
-<?php
+<?php include("connect.php");?>
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "corememories";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "SELECT * FROM islandsofpersonality";
-$result = $conn->query($sql);
-?>
 <!DOCTYPE html>
 <html lang="en">
 
